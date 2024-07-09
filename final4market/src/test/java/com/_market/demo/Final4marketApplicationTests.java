2681b9d845090494fe4c2424b1b0fe7de67c9084
juanalee <optihyuny07@gmail.com>
2024-07-09 15:01:35 +0900

    테스트

---
+++ final4market/src/test/java/com/_market/demo/Final4marketApplicationTests.java
@@ -0,0 +1,13 @@
+package com._market.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Final4marketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
